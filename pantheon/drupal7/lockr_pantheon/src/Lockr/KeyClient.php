6b9fd138002361a29ca0097e2492ee218eae62ca
Tynor Fujimoto <tynorf@gmail.com>
2016-01-24 17:07:54 -0800

    add reencrypt to pantheon drupal 7

--- pantheon/drupal7/lockr_pantheon/src/Lockr/KeyClient.php
+++ pantheon/drupal7/lockr_pantheon/src/Lockr/KeyClient.php
@@ -76,10 +76,14 @@ class KeyClient
      *
      * @return string Returns the decrypt data or true.
      */
-    public function set($name, $value, $label)
+    public function set($name, $value, $label, $encoded = null)
     {
         if ($this->encoded) {
-            list($value, $encoded) = $this->encrypt($value);
+            if ($encoded === NULL) {
+                list($value, $encoded) = $this->encrypt($value);
+            } else {
+                list($value, $encoded) = $this->reencrypt($value, $encoded);
+            }
         }
         $data = array(
             'key_value' => $value,
@@ -115,18 +119,43 @@ class KeyClient
         return '/v1/key/'.urlencode($name);
     }
 
+    protected function reencrypt($plaintext, $encoded)
+    {
+        list($cipher, $mode, $iv, $key) = $this->decode($encoded);
+
+        $ciphertext = mcrypt_encrypt($cipher, $key, $plaintext, $mode, $iv);
+
+        return array($ciphertext, $encoded);
+    }
+
     protected function encrypt($plaintext)
     {
-        $key = openssl_random_pseudo_bytes(32);
-
         $cipher = MCRYPT_RIJNDAEL_256;
         $mode = MCRYPT_MODE_CBC;
 
+        $key = openssl_random_pseudo_bytes(32);
         $iv_len = mcrypt_get_iv_size($cipher, $mode);
         $iv = mcrypt_create_iv($iv_len);
 
         $ciphertext = mcrypt_encrypt($cipher, $key, $plaintext, $mode, $iv);
+        $ciphertext = base64_encode($ciphertext);
+        $encoded = $this->encode($cipher, $mode, $iv, $key);
 
+        return array($ciphertext, $encoded);
+    }
+
+    protected function decrypt($ciphertext, $encoded)
+    {
+        list($cipher, $mode, $iv, $key) = $this->decode($encoded);
+        $ciphertext = base64_decode($ciphertext);
+
+        $key = mcrypt_decrypt($cipher, $key, $ciphertext, $mode, $iv);
+
+        return trim($key);
+    }
+
+    protected function encode($cipher, $mode, $iv, $key)
+    {
         $parts = array(
             $cipher,
             $mode,
@@ -134,21 +163,14 @@ class KeyClient
             base64_encode($key),
         );
 
-        $ciphertext = base64_encode($ciphertext);
-        $encoded = implode('$', $parts);
-
-        return array($ciphertext, $encoded);
+        return implode('$', $parts);
     }
 
-    protected function decrypt($ciphertext, $encoded)
+    protected function decode($encoded)
     {
         list($cipher, $mode, $iv, $key) = explode('$', $encoded, 4);
         $iv = base64_decode($iv);
-        $ciphertext = base64_decode($ciphertext);
         $key = base64_decode($key);
-
-        $key = mcrypt_decrypt($cipher, $key, $ciphertext, $mode, $iv);
-
-        return trim($key);
+        return array($cipher, $mode, $iv, $key);
     }
 }
