c80b51afd930bb2fc04bea6c61eb872c12768f39
cellar-door <chris@cellardoormediagroup.com>
2016-04-26 13:31:45 -0700

    Add WP specific function to make sure file is not accessed

--- lockr/src/Lockr/SiteClient.php
+++ lockr/src/Lockr/SiteClient.php
@@ -1,4 +1,11 @@
 <?php
+
+// Don't call the file directly and give up info!
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Lock it up!';
+	exit;
+}
+
 // ex: ts=4 sts=4 sw=4 et:
 
 namespace Lockr;
